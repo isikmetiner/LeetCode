afb3c53151782114eea66c2d1189e2b95db54447
Işık Metiner <isikmetiner0@gmail.com>
2022-12-21 10:13:40 +0300

    MergeTwoSortedLists

---
+++ LeetCode/LeetCodeProblems/MergeTwoSortedLists.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.LeetCodeProblems
+{
+    public class MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode firstList, ListNode secondList)
+        {
+            if (firstList == null)
+                return secondList;
+            if (secondList == null)
+                return firstList;
+
+            if (secondList.val > firstList.val)
+            {
+                firstList.next = MergeTwoLists(firstList.next, secondList);
+                return firstList;
+            }
+
+            secondList.next = MergeTwoLists(firstList, secondList.next);
+            return secondList;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
